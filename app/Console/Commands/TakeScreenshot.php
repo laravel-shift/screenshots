632bbf6905226594c82dde7e095aaf96d6851be6
Jason McCreary <jason@pureconcepts.net>
2022-02-11 18:28:10 -0500

    Generate one or all screenshots

--- app/Console/Commands/TakeScreenshot.php
+++ app/Console/Commands/TakeScreenshot.php
@@ -8,48 +8,26 @@ use Spatie\Browsershot\Browsershot;
 
 class TakeScreenshot extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'screenshot:take {shift} {path}';
+    protected $signature = 'screenshot {shift?}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Take a screenshot of the given Shift';
+    protected $description = 'Take screenshot of the given Shift or all Shifts';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
-        $shift = Shift::findOrFail($this->argument('shift'));
+        $shifts = is_null($this->argument('shift')) ? Shift::all()
+            : [Shift::findOrFail($this->argument('shift'))];
 
-        $html = view('shift.show', [
-            'shift' => $shift,
-            'view' => 'commits',
-        ])->render();
+        foreach ($shifts as $shift) {
+            $html = view('shift.show', [
+                'shift' => $shift,
+                'view' => 'commits',
+            ])->render();
 
-        Browsershot::html($html)
-            ->windowSize(1200, 628)
-            ->deviceScaleFactor(2)
-            ->save($this->argument('path'));
+            Browsershot::html($html)
+                ->windowSize(1200, 628)
+                ->deviceScaleFactor(2)
+                ->save(public_path('images/screenshots/' . $shift->sku . '.png'));
+        }
 
         return 0;
     }
