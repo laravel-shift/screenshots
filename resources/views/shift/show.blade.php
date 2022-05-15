b382e846fa4182d39f06521f4641691f5aa4ac35
Jason McCreary <jason@pureconcepts.net>
2022-05-15 12:06:41 -0400

    Change tag

--- resources/views/shift/show.blade.php
+++ resources/views/shift/show.blade.php
@@ -21,9 +21,9 @@ $source = Arr::random(['main', 'master', 'develop', 'dev']);
             <span class="ml-2 text-gray-600">
                 <span class="font-bold">jasonmccreary</span>
                 merged {{ $shift->commits->count() }} commits into
-                <code class="p-1 rounded-md bg-github-branch-bg text-github-branch-fg text-xs">{{ $source }}</code>
+                <span class="font-mono p-1 rounded-md bg-github-branch-bg text-github-branch-fg text-xs">{{ $source }}</span>
                 from
-                <code class="p-1 rounded-md bg-github-branch-bg text-github-branch-fg text-xs">shift-{{ $number }}</code>
+                <span class="font-mono p-1 rounded-md bg-github-branch-bg text-github-branch-fg text-xs">shift-{{ $number }}</span>
                 <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="inline-block fill-current">
                     <path fill-rule="evenodd" d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"></path><path fill-rule="evenodd" d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"></path>
                 </svg>
