a29614eaf41f9c4ecd3974c1524478744c70235f
Jason McCreary <jason@pureconcepts.net>
2023-04-01 09:24:02 -0400

    April Fools Shift

---
+++ resources/views/shift/skus/AF.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.pr')
+
+@section('description')
+<p>This pull request contains comments from a sarcastic <i>Shift bot</i> regarding outdated practices still detected even after upgrading over 85,000 Laravel applications. While they are left in jest, they are still real suggestions you might consider.</p>
+
+<p>Happy April Fools' Day from <a href="https://laravelshift.com">Shift</a>!</p>
+
+@endsection
