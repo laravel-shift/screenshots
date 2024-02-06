19b2c4f50b5a9f78dfd5218d64db50bdc0eaa173
Jason McCreary <jason@pureconcepts.net>
2024-02-06 13:51:21 -0500

    HTML Converter (#14)

--- resources/views/shift/skus/HC.blade.php
+++ resources/views/shift/skus/HC.blade.php
@@ -2,7 +2,7 @@
 
 @section('description')
 
-<p>This pull request includes the changes for upgrading to Laravel 9.x. Feel free to commit any additional changes to the <code>shift-{{ $number }}</code> branch.</p>
+<p>This pull request includes the changes for migrating from the <code>laravelcollective/html</code> package to the <code>spatie/laravel-html</code> package. Feel free to commit any additional changes to the <code>shift-{{ $number }}</code> branch.</p>
 
 <p><strong>Before merging</strong>, you need to:</p>
 
@@ -10,10 +10,8 @@
     <li class="mb-1">Checkout the <code>shift-{{ $number }}</code> branch</li>
     <li class="mb-1">Review <strong>all</strong> pull request comments for additional changes</li>
     <li class="mb-1">Run <code>composer update</code> (if the scripts fail, add <code>--no-scripts</code>)</li>
-    <li class="mb-1">Clear any config, route, or view cache</li>
+    <li class="mb-1">Clear any view cache</li>
     <li class="mb-1">Thoroughly test your application (<a class="text-blue-500" href="https://laravelshift.com/laravel-test-generator">no tests?</a>, <a class="text-blue-500" href="https://laravelshift.com/ci-generator">no CI?</a>)</li>
 </ul>
-
-<p>If you need help with your upgrade, check out the <a class="text-blue-500" href="https://laravelshift.com/human-shifts">Human Shifts</a>.</p>
 
 @endsection
