d3001837bcc27c6d19e1e4789512f9e2141ab366
Jason McCreary <jason@pureconcepts.net>
2024-02-06 14:06:14 -0500

    More tweaks

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 function markdown($content, $inline = true)
@@ -21,11 +22,15 @@ function commit_sha()
 function files_changed($shift)
 {
     if ($shift->isTailwind()) {
-        Arr::random([23, 39, 42, 56, 63]);
+        return Arr::random([23, 39, 42, 56, 63]);
     }
 
     if ($shift->sku === '10') {
         return 307;
+    }
+
+    if ($shift->sku === '11') {
+        return Arr::random(range(30, 63));
     }
 
     return Arr::random([63, 76, 82, 91, 103, 111, 123, 142, 256]);
@@ -44,6 +49,10 @@ function minus_changes($shift)
 {
     if ($shift->sku === '10') {
         return 1403;
+    }
+
+    if ($shift->sku === '11') {
+        return rand(1000, 2000);
     }
 
     return rand(80, 200);
