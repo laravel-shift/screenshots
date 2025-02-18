830984aebfd804d936aaa12bd47dfa550cf4727d
Shift <shift@laravelshift.com>
2025-02-18 19:11:12 +0000

    Add type hints from DocBlocks

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
 use App\Models\Shift;
 use Illuminate\Http\Request;
 
 class ShiftController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request): View
     {
         return view('shift.index', [
             'shifts' => Shift::all(),
         ]);
     }
 
-    public function show(Request $request, Shift $shift)
+    public function show(Request $request, Shift $shift): View
     {
         return view('shift.show', ['shift' => $shift]);
     }
