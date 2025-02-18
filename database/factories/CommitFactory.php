9253172409cd79e317f2c71bdc51301a7de82f2c
Shift <shift@laravelshift.com>
2025-02-18 19:11:12 +0000

    Remove redundant typing from DocBlocks

--- database/factories/CommitFactory.php
+++ database/factories/CommitFactory.php
@@ -8,8 +8,6 @@ class CommitFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
-     * @return array
      */
     public function definition(): array
     {
--- database/factories/ShiftFactory.php
+++ database/factories/ShiftFactory.php
@@ -8,8 +8,6 @@ class ShiftFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
-     * @return array
      */
     public function definition(): array
     {
