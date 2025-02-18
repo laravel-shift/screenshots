564c1a202abbdeb62136a292edc4b79fb9e75160
Shift <shift@laravelshift.com>
2025-02-18 19:04:38 +0000

    Remove unnecessary `$model` property

--- database/factories/CommitFactory.php
+++ database/factories/CommitFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CommitFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Commit::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
--- database/factories/ShiftFactory.php
+++ database/factories/ShiftFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class ShiftFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Shift::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
