c8b8e6f67e8265f51dceb35f355764d0842d7cfa
Jason McCreary <jason@pureconcepts.net>
2025-08-21 12:40:02 -0400

    Dusk Converter (#36)

--- database/seeders/ShiftsSeeder.php
+++ database/seeders/ShiftsSeeder.php
@@ -483,6 +483,22 @@ class ShiftsSeeder extends Seeder
             ['message' => 'Apply code style', 'info' => true],
         ]);
 
+        $shift = Shift::create([
+            'name' => 'Dusk Converter',
+            'sku' => 'DC',
+            'type' => 'Laravel',
+            'slug' => 'convert-laravel-dusk-to-pest-browser-tests',
+            'published_at' => '2025-08-21',
+        ]);
+        $shift->commits()->createMany([
+            ['message' => 'Add dependencies'],
+            ['message' => 'Convert Dusk test classes to Pest'],
+            ['message' => 'Convert to Pest Browser Testing API'],
+            ['message' => 'Rename `dusk` HTML attribute'],
+            ['message' => 'Remove core Dusk classes'],
+            ['message' => 'Swap test runner'],
+        ]);
+
         // Lumen
         $shift = Shift::create([
             'name' => 'Lumen to Laravel Shift',
